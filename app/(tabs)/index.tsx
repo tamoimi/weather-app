e653062929bc2657feea2893d0d255a63d9e8b8a
tami <tamiortami@gmail.com>
2024-08-21 13:58:20 +0900

    feat: flexBox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,93 +1,54 @@
-import { Image, StyleSheet, Platform, View, Text, SafeAreaView, TextInput } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function HomeScreen() {
-  {
-    /* test react-native ------------------------------------------------------------------------ */
-  }
-  const [text, onChangeText] = useState("Useless Text");
-  const [number, onChangeNumber] = useState("");
-
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.reactLogo} />}
-    >
-      {/* test react-native ------------------------------------------------------------------------ */}
-      <View>
-        <Text style={{ color: "white" }}>hello!!!👍</Text>
+    <View style={styles.container}>
+      {/* bar */}
+      <StatusBar style="light" />
+      {/* city */}
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
-
-      <SafeAreaView>
-        <TextInput style={styles.input} onChangeText={onChangeText} value={text} />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeNumber}
-          value={number}
-          placeholder="useless placeholder"
-          keyboardType="numeric"
-        />
-      </SafeAreaView>
-
-      {/* test react-native ------------------------------------------------------------------------ */}
-
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Tami!!!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">{Platform.select({ ios: "cmd + d", android: "cmd + m" })}</ThemedText> to
-          open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>Tap the Explore tab to learn more about what's included in this starter app.</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      {/* weather */}
+      <View style={styles.weather}>
+        {/* date */}
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
+  container: {
+    flex: 1,
+    backgroundColor: "cornflowerblue",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  weather: {
+    flex: 3,
   },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    borderColor: "#ccc",
-    borderRadius: 8,
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,6 +32,6 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
-  );
-}
+    </ThemeProvider>               
+  );                    
+}                      
